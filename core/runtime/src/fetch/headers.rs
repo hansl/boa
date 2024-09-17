440310c5d36daebbc5ff531590eddcaab652be35
Hans Larsen <hans@larsen.online>
2024-09-16 22:29:04 -0700

    .

--- core/runtime/src/fetch/headers.rs
+++ core/runtime/src/fetch/headers.rs
@@ -1,13 +1,40 @@
 //! The [`Headers`] JavaScript class.
 //!
 //! See <https://developer.mozilla.org/en-US/docs/Web/API/Headers>.
+use boa_engine::object::builtins::{JsArray, JsFunction};
 use boa_engine::value::Convert;
-use boa_engine::{js_error, Finalize, JsData, JsResult, JsString, Trace};
+use boa_engine::{js_error, Context, Finalize, JsData, JsResult, JsString, JsValue, Trace};
 use boa_interop::{js_class, JsClass};
 use http::header::HeaderMap as HttpHeaderMap;
-use http::HeaderName;
+use http::{HeaderName, HeaderValue};
 use std::str::FromStr;
 
+/// Converts a JavaScript string to a valid header name (or error).
+///
+/// # Errors
+/// If the key is not valid ASCII, an error is returned.
+#[inline]
+fn to_header_name(key: &JsString) -> JsResult<HeaderName> {
+    key.to_std_string()
+        .map_err(|_| ())
+        .and_then(|s| HeaderName::from_str(&s).map_err(|_| ()))
+        .map_err(|()| js_error!("Cannot convert key to header string as it is not valid ASCII."))
+}
+
+/// Converts a JavaScript string to a valid header value (or error).
+///
+/// # Errors
+/// If the value is not valid ASCII, an error is returned.
+#[inline]
+fn to_header_value(value: &JsString) -> JsResult<HeaderValue> {
+    value
+        .to_std_string()
+        .map_err(|_| ())
+        .and_then(|s| s.parse().map_err(|_| ()))
+        .map_err(|()| js_error!("Cannot convert value to header string as it is not valid ASCII."))
+}
+
+/// A JavaScript wrapper for the `Headers` object.
 #[derive(Debug, Clone, JsData, Trace, Finalize)]
 pub struct JsHeaders {
     #[unsafe_ignore_trace]
@@ -15,24 +42,43 @@ pub struct JsHeaders {
 }
 
 impl JsHeaders {
+    /// Appends a new value onto an existing header inside a Headers object,
+    /// or adds the header if it does not already exist.
+    ///
+    /// # Errors
+    /// If the key or value is not valid ASCII, an error is returned.
     pub fn append(&mut self, key: &JsString, value: &JsString) -> JsResult<()> {
-        let key = key
-            .to_std_string()
-            .map_err(|_| ())
-            .and_then(|s| HeaderName::from_str(&s).map_err(|_| ()))
-            .map_err(|_| {
-                js_error!("Cannot convert key to header string as it is not valid ASCII.")
-            })?;
-        let value = value
-            .to_std_string()
-            .map_err(|_| ())
-            .and_then(|s| s.parse().map_err(|_| ()))
-            .map_err(|_| {
-                js_error!("Cannot convert value to header string as it is not valid ASCII.")
-            })?;
-
+        let key = to_header_name(key)?;
+        let value = to_header_value(value)?;
         self.headers.append(key, value);
         Ok(())
+    }
+
+    /// Deletes a header from a Headers object.
+    ///
+    /// # Errors
+    /// If the key is not valid ASCII, an error is returned.
+    pub fn delete(&mut self, key: &JsString) -> JsResult<()> {
+        let key = to_header_name(key)?;
+        self.headers.remove(key);
+        Ok(())
+    }
+
+    /// Returns an iterator allowing to go through all key/value pairs contained in this object.
+    // TODO: This should return a JsIterator, but not such thing exists yet.
+    pub fn entries(&self, context: &mut Context) -> JsValue {
+        JsArray::from_iter(
+            self.headers
+                .iter()
+                .map(|(k, v)| {
+                    let k: JsValue = JsString::from(k.as_str()).into();
+                    let v: JsValue = JsString::from(v.to_str().unwrap_or("")).into();
+                    JsArray::from_iter([k, v], context).into()
+                })
+                .collect::<Vec<_>>(),
+            context,
+        )
+        .into()
     }
 }
 
@@ -44,8 +90,75 @@ js_class! {
             })
         }
 
-        fn append(this: JsClass<JsHeaders>, name: Convert<JsString>, value: Convert<JsString>) -> JsResult<()> {
+        fn append(
+            this: JsClass<JsHeaders>,
+            name: Convert<JsString>,
+            value: Convert<JsString>,
+        ) -> JsResult<()> {
             this.borrow_mut().append(&name.0, &value.0)
+        }
+
+        fn delete(
+            this: JsClass<JsHeaders>,
+            name: Convert<JsString>,
+        ) -> JsResult<()> {
+            this.borrow_mut().delete(&name.0)
+        }
+
+        fn entries(
+            this: JsClass<JsHeaders>,
+            context: &mut Context,
+        ) -> JsValue {
+            this.borrow().entries(context)
+        }
+
+        fn forEach(
+            this: JsClass<JsHeaders>,
+            callback: JsFunction,
+            this_arg: Option<JsValue>,
+        ) -> JsResult<()> {
+            unimplemented!("Headers.prototype.forEach")
+        }
+
+        fn get(
+            this: JsClass<JsHeaders>,
+            name: Convert<JsString>,
+        ) -> JsResult<JsValue> {
+            unimplemented!("Headers.prototype.get")
+        }
+
+        fn getSetCookie(
+            this: JsClass<JsHeaders>,
+            name: Convert<JsString>,
+        ) -> JsResult<JsValue> {
+            unimplemented!("Headers.prototype.get")
+        }
+
+        fn has(
+            this: JsClass<JsHeaders>,
+            name: Convert<JsString>,
+        ) -> JsResult<bool> {
+            unimplemented!("Headers.prototype.has")
+        }
+
+        fn keys(
+            this: JsClass<JsHeaders>,
+        ) -> JsValue {
+            unimplemented!("Headers.prototype.keys")
+        }
+
+        fn set(
+            this: JsClass<JsHeaders>,
+            name: Convert<JsString>,
+            value: Convert<JsString>,
+        ) -> JsResult<()> {
+            unimplemented!("Headers.prototype.set")
+        }
+
+        fn values(
+            this: JsClass<JsHeaders>,
+        ) -> JsValue {
+            unimplemented!("Headers.prototype.values")
         }
     }
 }
--- core/runtime/src/fetch/mod.rs
+++ core/runtime/src/fetch/mod.rs
@@ -10,12 +10,13 @@
 #[cfg(test)]
 mod tests;
 
-use crate::request::JsRequest;
+use crate::request::{JsRequest, RequestInit};
 use boa_engine::object::builtins::JsPromise;
 use boa_engine::property::Attribute;
 use boa_engine::realm::Realm;
-use boa_engine::{js_string, Context, JsResult, NativeObject};
+use boa_engine::{js_error, js_string, Context, JsObject, JsResult, JsString, NativeObject};
 use boa_interop::IntoJsFunctionCopied;
+use either::Either;
 use http::{Request as HttpRequest, Response as HttpResponse};
 
 pub mod headers;
@@ -23,35 +24,51 @@ pub mod request;
 
 /// A trait for backend implementation of an HTTP fetcher.
 // TODO: consider implementing an async version of this.
-pub trait Fetcher<Body>: NativeObject + Sized {
+pub trait Fetcher: NativeObject + Sized {
     /// Fetch an HTTP document, returning an HTTP response.
     ///
     /// # Errors
     /// Any errors returned by the HTTP implementation must conform to
     /// [`boa_engine::JsError`].
     fn fetch_blocking(
-        request: &HttpRequest<Body>,
+        &self,
+        request: &HttpRequest<&[u8]>,
         context: &mut Context,
-    ) -> JsResult<HttpResponse<Body>>;
+    ) -> JsResult<HttpResponse<Vec<u8>>>;
 }
 
 /// The `fetch` function.
-pub fn fetch<Body, T: Fetcher<Body>>(_context: &mut Context) -> JsPromise {
+pub fn fetch<T: Fetcher>(
+    resource: Either<JsString, JsObject>,
+    options: Option<RequestInit>,
+    context: &mut Context,
+) -> JsPromise {
+    let Some(fetcher) = context.get_data::<T>() else {
+        return JsPromise::reject(
+            js_error!(Error: "implementation of fetch requires a fetcher registered in the context"),
+            context,
+        );
+    };
+
+    // The resource parsing is complicated, so we parse it in Rust here (instead of relying on
+    // `TryFromJs` and friends).
+
+    let response = fetcher.fetch_blocking(&request, context);
     todo!()
 }
 
-pub fn register_fetch<F: Fetcher<()>>(
+pub fn register_fetch<F: Fetcher>(
     fetcher: F,
     realm: &Realm,
     context: &mut Context,
 ) -> JsResult<()> {
     context.register_global_class::<JsRequest>()?;
 
-    let fetch_fn = fetch::<(), F>
+    let fetch_fn = fetch::<F>
         .into_js_function_copied(context)
         .to_js_function(realm);
-    context.register_global_property(js_string!("fetch"), fetch_fn, Attribute::all())?;
     context.insert_data(fetcher);
+    context.register_global_property(js_string!("fetch"), fetch_fn, Attribute::all())?;
 
     Ok(())
 }
